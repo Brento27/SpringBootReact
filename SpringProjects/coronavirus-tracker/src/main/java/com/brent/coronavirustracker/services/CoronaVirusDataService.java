b7ad08cc9cc8bf04c9232e8265e0b25ac2bf964b
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-08-31 14:00:56 +0200

    Fixed common csv dependency issue

--- SpringProjects/coronavirus-tracker/src/main/java/com/brent/coronavirustracker/services/CoronaVirusDataService.java
+++ SpringProjects/coronavirus-tracker/src/main/java/com/brent/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,9 @@
 package com.brent.coronavirustracker.services;
 
 //import com.brent.coronavirustracker.models.LocationStats;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,13 +13,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
 
     @PostConstruct
