c5231d081d599496f758cdfaa53ffdf4d7e069f3
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-09-13 21:45:19 +0200

    vid23
    Implement GetAllUsers

---
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.brent.restservices.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brent.restservices.entities.User;
+import com.brent.restservices.services.UserService;
+
+//Controller
+@RestController
+public class UserController {
+	
+	//Autowire the UserService
+	private UserService userService;
+	
+	//getAllUsers Method
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+
+}
---
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/services/UserService.java
@@ -0,0 +1,25 @@
+package com.brent.restservices.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brent.restservices.entities.User;
+import com.brent.restservices.repositories.UserRepository;
+
+//Service
+@Service
+public class UserService {
+
+	//Autowire the UserRepository
+	@Autowired
+	private UserRepository userRepository;
+	
+	//getAllUsers
+	public List<User> getAllUsers(){
+		
+		return userRepository.findAll();
+		
+	}
+}
