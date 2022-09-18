fed381f0959640b02c521315ec75f5f9cbc07190
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-09-18 21:09:14 +0200

    vid 27
    updateUserById

--- SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/UserController.java
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +40,11 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public Optional<User> getUserById(@PathVariable("id") Long id) {
 		return userService.getUserById(id);
+	}
+	
+	//UpdateUserById
+	@PutMapping("/users/{id}")
+	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
+		return userService.updateUserById(id, user);
 	}
 }
