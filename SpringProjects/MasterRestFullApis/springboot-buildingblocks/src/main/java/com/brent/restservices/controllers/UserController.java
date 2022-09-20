2bf6d9e423744dd60ed19ed398162b5004ecc250
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-09-20 08:26:19 +0200

    vid 33
    ResponseStatusException for GetUserById

--- SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/UserController.java
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.brent.restservices.entities.User;
+import com.brent.restservices.exceptions.UserNotFoundException;
 import com.brent.restservices.services.UserService;
 
 //Controller
@@ -40,7 +43,13 @@ public class UserController {
 	//getUserById
 	@GetMapping("/users/{id}")
 	public Optional<User> getUserById(@PathVariable("id") Long id) {
-		return userService.getUserById(id);
+		
+		try {
+			return userService.getUserById(id);
+		} catch(UserNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+		}
+		
 	}
 	
 	//UpdateUserById
