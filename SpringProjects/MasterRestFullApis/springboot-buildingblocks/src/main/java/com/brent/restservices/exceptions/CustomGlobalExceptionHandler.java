bfcf7861aa19bf1a1365c657255dec7582be53c0
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-09-21 11:50:08 +0200

    vid 44
    Implement HttpRequestMethodNotSupportedException

--- SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/exceptions/CustomGlobalExceptionHandler.java
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/exceptions/CustomGlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -13,15 +14,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	//MethodArgumentNotValidException
+	// MethodArgumentNotValidException
 	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
-		
-		CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(), "From MethodArgumentNotValid Exception in GEH", ex.getMessage());
-		
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
+				"From MethodArgumentNotValid Exception in GEH", ex.getMessage());
+
 		return new ResponseEntity<>(customErrorDetails, HttpStatus.BAD_REQUEST);
 	}
-	
-	
+
+	// HttpRequestMethodNotSupportedException
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
+				"From HttpRequestMethodNotSupported Exception in GEH - Method not allowed", ex.getMessage());
+
+		return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
+
+	}
 }
