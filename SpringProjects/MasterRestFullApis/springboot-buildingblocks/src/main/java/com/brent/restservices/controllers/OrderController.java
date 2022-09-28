ce833c0717221d6b2a900cb99e8ff4106ee7ab55
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-09-28 10:41:21 +0200

    vid 56
    Implement getOrderByOrderId

--- SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/OrderController.java
+++ SpringProjects/MasterRestFullApis/springboot-buildingblocks/src/main/java/com/brent/restservices/controllers/OrderController.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brent.restservices.entities.Order;
 import com.brent.restservices.entities.User;
 import com.brent.restservices.exceptions.UserNotFoundException;
+import com.brent.restservices.repositories.OrderRepository;
 import com.brent.restservices.repositories.UserRepository;
 
 @RestController
@@ -20,6 +23,9 @@ public class OrderController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 	
 	//get All Orders for a user
 	@GetMapping("/{userid}/orders")
@@ -34,4 +40,35 @@ public class OrderController {
 		
 	}
 	
+	//Create Order
+	@PostMapping("{userid}/orders")
+	public Order createOrder(@PathVariable Long userid, @RequestBody Order order) throws UserNotFoundException {
+		
+		Optional<User> userOptional = userRepository.findById(userid);
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
+		
+		User user = userOptional.get();
+		order.setUser(user);
+		return orderRepository.save(order);
+	}
+	
+	//GetOrderByOrderId
+	@GetMapping("/{userid}/orders/{orderid}")
+	public Order getOrderById(@PathVariable Long userid, @PathVariable Long orderid) throws UserNotFoundException {
+		
+		Optional<User> userOptional = userRepository.findById(userid);
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
+		
+		Optional<Order> orderOptional = orderRepository.findById(orderid);
+		if(!orderOptional.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
+		
+		Order order = orderOptional.get();
+		return order;
+	}
 }
