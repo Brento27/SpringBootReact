692361f9d95b37cf89b08ec3925111e50ef0c922
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-11-04 10:31:03 +0200

    15. Password Hashing (Backend)

--- SpringProjects/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
+++ SpringProjects/hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserService.java
@@ -2,19 +2,24 @@ package com.hoaxify.hoaxify.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	BCryptPasswordEncoder passwordEncoder;
 
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
 	public User save(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 
--- SpringProjects/hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ SpringProjects/hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.hoaxify.hoaxify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,6 +53,15 @@ public class UserControllerTest {
 		
 		assertThat(userRepository.count()).isEqualTo(1);
 	}
+	
+	@Test
+	public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+		User user = createValidUser();
+		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		List<User> users = userRepository.findAll();
+		User inDB = users.get(0);
+		assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
+		}
 	
 	@Test
 	public void postUser_whenUserIsValid_receiveSuccessMessage() {
